18330ceefb7c82da8dcaa3cd8e402e446a216a3b
Ethan Chen <Ethanchen4@gmail.com>
2025-02-18 01:24:00 -0500

    created component for mouse ranking

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,19 +1,14 @@
+import MouseRanking from "@/components/shared/ranking/mouse-ranking";
 import VideoList from "@/components/shared/video/video-list";
 import { testData } from "@/db/test-data";
 import { getMouseRankings } from "@/lib/actions/mouse-actions";
 
 export default async function Homepage() {
   const mouseRankings = await getMouseRankings();
-  console.log(mouseRankings);
   return (
     <>
       <VideoList data={testData} title="Latest Videos" />
-      <h2 className="h2-bold">Mouse Ranking</h2>
-      <ol className="list-decimal pl-4">
-        {mouseRankings.map((mouse) => {
-          return <li key={mouse.id}>{mouse.company + " " + mouse.name}</li>;
-        })}
-      </ol>
+      <MouseRanking data={mouseRankings} />
     </>
   );
 }
---
+++ components/shared/ranking/mouse-ranking.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Crown, Medal } from "lucide-react";
+
+export default function MouseRanking({ data }: { data: any }) {
+  return (
+    <div>
+      <h2 className="h2-bold">Mouse Ranking</h2>
+      <ol className="list-decimal pl-4">
+        {data.map((mouse: any) => {
+          if (mouse.rank === 1) {
+            return (
+              <li key={mouse.id} className="text-lg text-yellow-500">
+                <div className="flex">
+                  <p>{mouse.company + " " + mouse.name}</p>
+                  <Crown className="ml-1 place-self-center" />
+                </div>
+              </li>
+            );
+          } else if (mouse.rank === 2) {
+            return (
+              <li key={mouse.id} className="text-lg text-neutral-500">
+                <p>{mouse.company + " " + mouse.name}</p>
+              </li>
+            );
+          } else if (mouse.rank === 3) {
+            return (
+              <li key={mouse.id} className="text-lg text-orange-800">
+                <p>{mouse.company + " " + mouse.name}</p>
+              </li>
+            );
+          }
+          return (
+            <li key={mouse.id} className="text-sm">
+              {mouse.company + " " + mouse.name}
+            </li>
+          );
+        })}
+      </ol>
+    </div>
+  );
+}
