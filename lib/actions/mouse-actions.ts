3b2419e3ca8102af9c64e634b5804e0a3a0a533b
Ethan Chen <Ethanchen4@gmail.com>
2025-02-18 23:47:14 -0500

    setup serverless environment configuration

--- lib/actions/mouse-actions.ts
+++ lib/actions/mouse-actions.ts
@@ -1,14 +1,13 @@
 "use server";
-import { PrismaClient } from "@prisma/client";
 import { MOUSE_RANKING_LIMIT } from "../constants";
+import { prisma } from "@/db/prisma";
+import { convertToPlainObject } from "../utils";
 
 export async function getMouseRankings() {
-  const prisma = new PrismaClient();
-
   const data = await prisma.mouse.findMany({
     take: MOUSE_RANKING_LIMIT,
     orderBy: { rank: "asc" },
   });
 
-  return data;
+  return convertToPlainObject(data);
 }
