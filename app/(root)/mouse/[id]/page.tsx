776521f6b4518176167b6c32c29508a7e04e2101
Ethan Chen <Ethanchen4@gmail.com>
2025-02-20 00:03:48 -0500

    added styling to crown on rank 1 page

--- app/(root)/mouse/[id]/page.tsx
+++ app/(root)/mouse/[id]/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { getMouseByID } from "@/lib/actions/mouse-actions";
 import { notFound } from "next/navigation";
+import { Crown } from "lucide-react";
 
 export default async function MouseDetailsPage(props: {
   params: Promise<{ id: string }>;
@@ -18,7 +19,14 @@ export default async function MouseDetailsPage(props: {
             <div className="flex flex-col gap-2">
               <p>{mouse.company}</p>
               <h1 className="h3-bold">{mouse.name}</h1>
-              <p>Mausbert Rank: {mouse.rank}</p>
+              <div className="flex">
+                <p>Mausbert Rank: {mouse.rank} </p>
+                {mouse.rank === 1 ? (
+                  <Crown className="ml-2 self-center text-yellow-500" />
+                ) : (
+                  <></>
+                )}
+              </div>
             </div>
           </div>
           <div className="col-span-1"></div>
