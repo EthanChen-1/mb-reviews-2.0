fcc7a1c9002636673a4c0783763932a04afdb654
Ethan Chen <Ethanchen4@gmail.com>
2025-02-24 19:01:46 -0500

    added user based models

--- lib/validators.ts
+++ lib/validators.ts
@@ -1,6 +1,5 @@
 import { z } from "zod";
 
-//Schem for inserting Mouse
 export const insertMouseSchema = z.object({
   name: z.string().min(1, "Name must be at least 1 character"),
   slug: z.string().min(1, "Slug must contain at least 1 character"),
